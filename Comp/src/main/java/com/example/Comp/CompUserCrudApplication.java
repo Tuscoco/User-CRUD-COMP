ba78dbe0df27dbdf9c0bf3c71b5b86fdadc64886
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2025-05-23 07:32:05 -0300

    Adicionando arquivos iniciais

---
+++ Comp/src/main/java/com/example/Comp/CompUserCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Comp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompUserCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompUserCrudApplication.class, args);
+	}
+
+}
---
+++ Comp/src/test/java/com/example/Comp/CompUserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Comp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompUserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
